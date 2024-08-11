8ed89b3577d5080b61b49c80e03266e79bd3f5d0
Dhilan Fye <dhilanfye@MacBook-Pro-473.local>
2024-08-11 19:23:14 -0400

    Update config to use DATABASE_URL from environment

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://healthcare_user:pens@localhost/healthcare_db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
