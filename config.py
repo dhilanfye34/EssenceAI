cc3f393f070307bb8b7d5107146180d90d894404
Dhilan Fye <dhilanfye@MacBook-Pro-473.local>
2024-08-11 20:08:28 -0400

    Added Heroku Scheduler for fetching articles

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://healthcare_user:pens@localhost/healthcare_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
