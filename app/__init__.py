024f2d4ee6bc9dff2cc4d07d552d8fab2fd117d4
Dhilan Fye <dhilanfye@MacBook-Pro-473.local>
2024-08-19 19:09:39 -0400

    Fixed config import path

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -7,7 +8,7 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.from_object(Config)
 
     db.init_app(app)
     migrate.init_app(app, db)
