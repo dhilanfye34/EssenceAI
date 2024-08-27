68953e078b2f66f02ce73a50f2655153def2f5c7
Dhilan Fye <dhilanfye@MacBook-Pro-551.local>
2024-08-26 20:17:10 -0400

    Deploying to Heroku with Heroku DB

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('postgres://u87ggelhtpapvq:p31a412b46a97659c006b2b2522201bc7370c18ff081658f9b67833a287110621@c3gtj1dt5vh48j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dcmogq6mst0e98').replace("postgres://", "postgresql://", 1) + "?sslmode=require"
+    # Directly use the database URL and ensure the format is correct
+    raw_db_uri = os.getenv('DATABASE_URL', 'postgres://u87ggelhtpapvq:p31a412b46a97659c006b2b2522201bc7370c18ff081658f9b67833a287110621@c3gtj1dt5vh48j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dcmogq6mst0e98')
+    
+    # Replace "postgres://" with "postgresql://" if necessary
+    SQLALCHEMY_DATABASE_URI = raw_db_uri.replace("postgres://", "postgresql://", 1) + "?sslmode=require"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
