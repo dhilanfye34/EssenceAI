612626560488c3c0d646356ce8a0f3e2f5b3650c
Simon <20sianjave04@gmail.com>
2024-09-23 11:19:28 -0400

    email update

--- app/tasks.py
+++ app/tasks.py
@@ -6,11 +6,11 @@ import smtplib
 def send_email_async(recipient_email):
     # Example email sending function using SMTP
     msg = MIMEText('This is the email content')
-    msg['Subject'] = 'Your Subject'
-    msg['From'] = 'youremail@example.com'
+    msg['Subject'] = 'EssenceAI'
+    msg['From'] = 'aiessence86@gmail.com'
     msg['To'] = recipient_email
 
     with smtplib.SMTP('smtp.mailserver.com', 587) as server:
         server.starttls()  # Secure the connection
-        server.login('your_email_username', 'your_password')
-        server.sendmail('youremail@example.com', recipient_email, msg.as_string())
+        server.login('aiessence86@gmail.com', 'WEssence1!')
+        server.sendmail('aiessence86@gmail.com', recipient_email, msg.as_string())
