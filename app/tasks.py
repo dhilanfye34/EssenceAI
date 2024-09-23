0a951656eb63c621b63e549f837993e50b6de609
Simon <20sianjave04@gmail.com>
2024-09-23 11:24:36 -0400

    new email

--- app/tasks.py
+++ app/tasks.py
@@ -10,7 +10,12 @@ def send_email_async(recipient_email):
     msg['From'] = 'aiessence86@gmail.com'
     msg['To'] = recipient_email
 
-    with smtplib.SMTP('smtp.mailserver.com', 587) as server:
-        server.starttls()  # Secure the connection
-        server.login('aiessence86@gmail.com', 'WEssence1!')
-        server.sendmail('aiessence86@gmail.com', recipient_email, msg.as_string())
+   
+    try:
+        with smtplib.SMTP('smtp.mailserver.com', 587) as server:
+            server.starttls()  # Secure the connection
+            server.login('aiessence86@gmail.com', 'WEssence1!')
+            server.sendmail('aiessence86@gmail.com', recipient_email, msg.as_string())
+        print(f"Email sent to {recipient_email}")
+    except Exception as e:
+        print(f"Failed to send email to {recipient_email}: {e}")
